90e2d6661b34601ed2b7b4789a9b23a0be47d826
cesaroiac <caiocesarama@gmail.com>
2024-07-11 11:50:43 -0300

    Demonstração do uso e regras de PropTypes e DefaultProps

--- src/components/List.js
+++ src/components/List.js
@@ -6,9 +6,10 @@ function List(){
         <>
             <h1>Minha lista</h1>
             <ul>
-                <Item marca="Ferrari"/>
-                <Item marca="lamborguini"/>
-                <Item marca="Honda"/>
+                <Item marca="Ferrari" ano_lancamento={1985}/>
+                <Item marca="lamborguini" ano_lancamento={1990}/>
+                <Item marca="Honda" ano_lancamento={1987}/>
+                <Item />
             </ul>
         </>
     )
